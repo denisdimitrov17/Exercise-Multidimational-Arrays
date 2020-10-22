cc52904478991429990c7ff3aa89ed0c792e9195
denisdimitrov17 <71427957+denisdimitrov17@users.noreply.github.com>
2020-10-22 22:13:08 +0300

    Add files via upload

---
+++ 2. Squares in Matrix/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = n[0];
+            int cols = n[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            FillMatrix(matrix);
+            int count = 0;
+
+            for (int row = 0; row < rows -1; row++)
+            {
+                for (int col = 0; col < cols -1; col++)
+                {
+                    char currentElement = matrix[row, col];
+                    if (currentElement == matrix[row, col + 1]
+                        && currentElement == matrix[row + 1, col]
+                        && currentElement == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] matrixRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = matrixRow[col];
+                }
+            }
+        }
+    }
+}
